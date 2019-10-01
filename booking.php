295fff7d87501cc6843ac9d5c77dc45ceff6ffe9
sourcecde <dbrj.prof@gmail.com>
2019-10-01 11:22:36 +0530

    Booking

--- booking.php
+++ booking.php
@@ -1,14 +1,14 @@
 <?php 
 	include('includes/header.php');
-	include('includes/dbconnect.php');
+	// include('includes/dbconnect.php');
 
-	if($_SERVER['REQUEST_METHOD'] == 'GET') {
-		$q = $_GET['q'];
-		$sql = "SELECT * FROM tbl_purohit INNER JOIN tbl_location ON tbl_purohit.location_id = tbl_location.id AND tbl_location.name LIKE '".$q."'";
-		$result = mysqli_query($con,$sql);
-		$num_rows = mysqli_num_rows($result);
+	// if($_SERVER['REQUEST_METHOD'] == 'GET') {
+	// 	$q = $_GET['q'];
+	// 	$sql = "SELECT * FROM tbl_purohit INNER JOIN tbl_location ON tbl_purohit.location_id = tbl_location.id AND tbl_location.name LIKE '".$q."'";
+	// 	$result = mysqli_query($con,$sql);
+	// 	$num_rows = mysqli_num_rows($result);
 
-	}
+	// }
 ?>
 
 
@@ -30,7 +30,7 @@
 							</div>
 							<div class="span7" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
 								<!-- BEGIN MAIN NAVIGATION -->
-								<nav class="nav nav__primary clearfix">
+								<!-- <nav class="nav nav__primary clearfix">
 									<div class="search-form__h hidden-phone clearfix">
 										<a href="#"><i class="icon-search"></i>Search</a>
 										<form id="search-header" class="navbar-form pull-right" method="post" action="index.php" accept-charset="utf-8">
@@ -39,7 +39,7 @@
 										</form>
 									</div>
 									
-								</nav>
+								</nav> -->
 								<!-- END MAIN NAVIGATION -->
 							</div>
 						</div>
@@ -65,52 +65,11 @@
 									<div class="row">
 										<div class="span12">
 											<div class="title-box clearfix ">
-												<?php if($num_rows){?>
-												<h2 class="title-box_primary">Search Results</h2>
-												<h3 class="title-box_secondary"><?php echo $q; ?></h3>
-												<?php }else{?>
-												<h2 class="title-box_primary">No Result Found</h2>
-												<?php }?>
+												<h2 class="title-box_primary">Purohit Booking</h2>
+												<!-- <h3 class="title-box_secondary">second heding</h3> -->
 											</div>
 											<!-- //.title-box -->
 										</div>
-
-										<?php 
-											while($row = mysqli_fetch_array($result)){
-										?>
-
-										<div class="span3">
-											<div class="service-box services-home-two">
-												<figure class="icon">
-													<img src="images/chanakya.png" alt="" />
-												</figure>
-												<div class="service-box_body">
-													<h2 class="title"><?php echo $row[1]; ?></h2>
-													<div class="service-box_txt">
-														Contact : <?php echo $row[5]; ?><br>
-														<!-- Address : <?php echo $row[3]; ?><br> -->
-														<!-- PIN : <?php echo $row[4]; ?><br> -->
-														Age : <?php echo $row[2]; ?>
-														Grade : <?php echo $row[7]; ?>
-														
-														<div>
-															<a href="<?php echo 'booking.php?id='.$row[0]; ?>">
-															<input class="btn-primary" type="submit" value="Book">
-															</a>
-														</div>
-													</div>
-
-												</div>
-											</div>
-											<!-- /Service Box -->
-										</div>
-
-
-										<?php
-											}
-										?>
-
-
 									</div>
 
 
@@ -122,15 +81,44 @@
 									<div class="darker-box pad-2">
 										<div class="row">
 											<div class="span6">
-												<p><iframe src="//player.vimeo.com/video/27551252?title=0&amp;byline=0&amp;portrait=0" height="313" width="560" allowfullscreen="" frameborder="0"></iframe></p>
+												<p><figure class="icon">
+													<img src="images/QR.png" alt="" />
+												</figure></p>
 											</div>
 											<div class="span6">
 												<div class="service-box services-home-three">
 													<div class="service-box_body">
-														<h2 class="title">The Hindu Temple </h2>
-														<h5 class="sub-title">is a greatest achievement and an asset of Hindu community in the area. Your contribution helps the organization to serve our community better. Please take the step today. </h5>
-														<div class="service-box_txt">Quisque nulla. Vestibulum libero nislalesu. Vivamus eget nibh. Etiam cursus leo vel metus. Nulla facilisi. Aenean eros. Vestibunte ipsum primis in faucibus orci luctus et ultrices posuere a Curae; Suspendisse sollicitudin veed leo. Ut pharetra augue nec augue. Nam elit magna. </div>
-														<div class="btn-align"><a href="https://livedemo00.template-help.com/wordpress_48550/news-media/" title="view all videos" class="btn btn-inverse btn-normal btn-primary " target="_self">view all videos</a></div>
+														<h2 class="title">Payment method</h2>
+														<h5 class="sub-title">Account transfer</h5>
+														<div class="service-box_txt">
+															Name : Fistname Lastname<br>
+															A/C : 89798155165484651644553<br>
+															IFSC : BNK00972
+														</div>
+
+														<h5 class="sub-title">UPI</h5>
+														<div class="service-box_txt">
+															VPA : username007@paytm<br>
+														</div>
+
+														<h5 class="sub-title">Confirm Payment</h5>
+														<div class="service-box_txt">
+															<div id="newsletterwidget-2" class="visible-all-devices ">
+																<div class="newsletter newsletter-widget">
+																	<form action="" method="post">
+																		<p>
+																			<input class="newsletter-email" type="text" required name="ne" placeholder="transaction number" />
+																		</p>
+																		<p>
+																			<input class="newsletter-submit" type="submit" value="Confirm"/>
+																		</p>
+																	</form>
+																</div>
+															</div>
+														</div>
+
+
+														<!-- <div class="btn-align"><a href="https://livedemo00.template-help.com/wordpress_48550/news-media/" title="view all videos" class="btn btn-inverse btn-normal btn-primary " target="_self">view all videos</a></div> -->
 													</div>
 												</div>
 												<!-- /Service Box -->
@@ -155,15 +143,7 @@
 				<div class="row">
 					<div class="span12" data-motopress-wrapper-file="wrapper/wrapper-footer.php" data-motopress-wrapper-type="footer" data-motopress-id="5d695813f006f">
 						<div class="row footer-widgets">
-							<div class="span7 border-padding">
-								<div data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-1">
-									<div id="text-5" class="">
-										<h4>Quisque nulla libero porta</h4>
-										<div class="textwidget">Vestibulum iaculis lacinia est. Proin dictum elemtum velit. Fequat ante. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque sed dolor. Aliquam congueentum nisl. Mauris accumsnulla vel diam. Sed in lacus ut enim adipiscing aliquet. In pede mi, aliquet sit amet, smod in, auctor ut, ligula. 
-											Aliquam dapibus tincidunt metus. 
-										</div>
-									</div>
-								</div>
+							<div class="border-padding">
 								<div class="social-nets-wrapper bottom" data-motopress-type="static" data-motopress-static-file="static/static-social-networks.php">
 									<ul class="social">
 										<li class="twitter"><a href="#" title="twitter"><i class="icon-twitter"></i></a></li>
@@ -175,62 +155,13 @@
 									</ul>
 								</div>
 							</div>
-							<div class="span5" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-2">
-								<div id="newsletterwidget-2" class="visible-all-devices ">
-									<h4>Newsletter signup</h4>
-									Latest information only. Day by day
-									<script type="text/javascript">
-										//<![CDATA[
-										if (typeof newsletter_check !== "function") {
-										window.newsletter_check = function (f) {
-										    var re = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-]{1,})+\.)+([a-zA-Z0-9]{2,})+$/;
-										    if (!re.test(f.elements["ne"].value)) {
-										        alert("The email is not correct");
-										        return false;
-										    }
-										    if (f.elements["ny"] && !f.elements["ny"].checked) {
-										        alert("You must accept the privacy statement");
-										        return false;
-										    }
-										    return true;
-										}
-										}
-										//]]>
-									</script>
-									<div class="newsletter newsletter-widget">
-										<script type="text/javascript">
-											//<![CDATA[
-											if (typeof newsletter_check !== "function") {
-											window.newsletter_check = function (f) {
-											    var re = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-]{1,})+\.)+([a-zA-Z0-9]{2,})+$/;
-											    if (!re.test(f.elements["ne"].value)) {
-											        alert("The email is not correct");
-											        return false;
-											    }
-											    if (f.elements["ny"] && !f.elements["ny"].checked) {
-											        alert("You must accept the privacy statement");
-											        return false;
-											    }
-											    return true;
-											}
-											}
-											//]]>
-										</script>
-										<form action="https://livedemo00.template-help.com/wordpress_48550/wp-content/plugins/newsletter/do/subscribe.php" onsubmit="return newsletter_check(this)" method="post">
-											<input type="hidden" name="nr" value="widget"/>
-											<p><input class="newsletter-email" type="email" required name="ne" value="enter your e-mail address" onclick="if (this.defaultValue==this.value) this.value=''" onblur="if (this.value=='') this.value=this.defaultValue"/></p>
-											<p><input class="newsletter-submit" type="submit" value="Sign Up"/></p>
-										</form>
-									</div>
-								</div>
-							</div>
 						</div>
 						<div class="row">
 							<div class="span12 footer-info-wrap" data-motopress-type="static" data-motopress-static-file="static/static-footer-info.php">
 								<div class="footer-info">
 									<div class="label-name">Feel free to contact us:</div>
 									<div class="phone"><i class="icon-phone"></i>+1 959 603 6035</div>
-									<div class="mail"><i class="icon-envelope-alt"></i><a href="/cdn-cgi/l/email-protection#4d202c24210d292820222124232663223f2a"><span class="__cf_email__" data-cfemail="ee838f8782ae8a8b838182878085c0819c89">[email&#160;protected]</span></a></div>
+									<div class="mail"><i class="icon-envelope-alt"></i><a href="/cdn-cgi/l/email-protection#4d202c24210d292820222124232663223f2a"><span class="__cf_email__" data-cfemail="ee838f8782ae8a8b838182878085c0819c89">support@epurohit.com</span></a></div>
 								</div>
 							</div>
 						</div>
